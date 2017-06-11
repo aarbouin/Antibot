a40f2dfa0acbfacb9bc6e7de169b6fd8c352a936
Jean <jgiard@antidot.net>
2017-06-11 23:10:56 +0200

    fix plugins arg filter

--- antibot/flow/bootstrap.py
+++ antibot/flow/bootstrap.py
@@ -11,8 +11,8 @@ from pynject import pynject
 def find_plugins(configuration: Configuration, plugin_filter: List[str]):
     plugins = load(configuration.plugins_package, subclasses=AntibotPlugin)
     for plugin in plugins:
-        if len(plugin_filter) > 0 and plugin.__name__ in plugin_filter:
-            print(plugin.__name__)
-            logging.info('found plugin {}'.format(plugin.__name__))
-            pynject(plugin)
-            yield plugin
+        if len(plugin_filter) > 0 and plugin.__name__ not in plugin_filter:
+            continue
+        logging.info('found plugin {}'.format(plugin.__name__))
+        pynject(plugin)
+        yield plugin
