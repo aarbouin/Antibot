ba78389c2ad55d6de89d63399489c2b8d8c5f161
Jean Giard <jgiard@antidot.net>
2019-06-13 17:09:24 +0200

    add some logs to debug slack commands

--- src/antibot/backend/request_checker.py
+++ src/antibot/backend/request_checker.py
@@ -1,5 +1,6 @@
 import hashlib
 import hmac
+import logging
 import time
 
 from bottle import BaseRequest, abort
@@ -14,13 +15,20 @@ class RequestChecker:
         self.configuration = configuration
 
     def check_request(self, request: BaseRequest):
-        timestamp = request.headers['X-Slack-Request-Timestamp']
-        if abs(time.time() - float(timestamp)) > 60 * 5:
-            abort(401, 'Invalid timestamp')
-        body = request.body.read()
-        data = 'v0:{}:'.format(timestamp).encode() + body
-        request_hash = 'v0=' + hmac.new(str.encode(self.configuration.signing_secret),
-                                        data, hashlib.sha256).hexdigest()
-        signature = request.headers['X-Slack-Signature']
-        if not hmac.compare_digest(request_hash, signature):
-            abort(401)
+        try:
+            timestamp = request.headers['X-Slack-Request-Timestamp']
+            if abs(time.time() - float(timestamp)) > 60 * 5:
+                abort(401, 'Invalid timestamp')
+            body = request.body.read()
+            data = 'v0:{}:'.format(timestamp).encode() + body
+            request_hash = 'v0=' + hmac.new(str.encode(self.configuration.signing_secret),
+                                            data, hashlib.sha256).hexdigest()
+            signature = request.headers['X-Slack-Signature']
+            if not hmac.compare_digest(request_hash, signature):
+                abort(401)
+        except KeyError:
+            logging.error('Could not verify request')
+            logging.error(str(dict(request.headers)))
+            logging.error(str(dict(request.environ)))
+            logging.error(str(dict(request.params)))
+            abort(401, 'no X-Slack headers found')
