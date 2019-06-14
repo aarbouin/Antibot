14c8f74a3320d1c1a6c89168546f3492db20927c
Jean Giard <jgiard@antidot.net>
2019-06-14 09:39:07 +0200

    fix bug with old users in db

--- src/antibot/model/user.py
+++ src/antibot/model/user.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
+
 class User:
-    def __init__(self, id: str, display_name: str, email: str):
+    def __init__(self, id: str, display_name: str, email: Optional[str] = None):
         self.id = id
         self.display_name = display_name
         self.email = email
